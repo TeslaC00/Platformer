598e28440b7cc95cc0d42008d308b84046594e46
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2024-12-28 19:20:54 +0530

    feat: added zoom in

--- systems/camera.lua
+++ systems/camera.lua
@@ -13,17 +13,19 @@ function Camera.new(x, y)
     }, Camera)
 end
 
+-- TODO: This variable and function not used, remove it or use it
 function Camera:setWidthLimit(limitW)
     self.lw = limitW
 end
 
+-- TODO: This variable and function not used, remove it or use it
 function Camera:setHeightLimit(limitH)
     self.lh = limitH
 end
 
 function Camera:setPosition(x, y)
-    self.x = x
-    self.y = y
+    self.x = x * self.scaleX
+    self.y = y * self.scaleY
 end
 
 function Camera:apply()
@@ -35,6 +37,16 @@ function Camera:apply()
     love.graphics.rotate(self.rotation)
 end
 
+function Camera:zoomIn()
+    self.scaleX = self.scaleX + 0.1
+    self.scaleY = self.scaleY + 0.1
+end
+
+function Camera:zoomOut()
+    self.scaleX = self.scaleX - 0.1
+    self.scaleY = self.scaleY - 0.1
+end
+
 function Camera:remove()
     love.graphics.pop()
 end
@@ -42,6 +54,8 @@ end
 function Camera:reset()
     self.x = self.startX
     self.y = self.startY
+    self.scaleX = 1
+    self.scaleY = 1
 end
 
 return Camera
